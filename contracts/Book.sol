b4f4ec0de2234428c6c53e27610e3fc74dc90388
Gustavo <gustavo@obex.pw>
2024-01-18 21:46:48 -0300

    started the project

---
+++ contracts/Book.sol
@@ -0,0 +1,16 @@
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol;
+
+contract Book {
+    struct Order {
+        uint256 price;
+        uint256 amount;
+    }
+
+    constructor(address base, address quote) {
+        
+    }
+
+    place() {
+        
+    }
+}
